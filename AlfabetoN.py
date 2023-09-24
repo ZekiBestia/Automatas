de8d50f87d45e2e31e3ad1a48a991ef3c0b77df7
ZekiBestia <ezequielor73@gmail.com>
2023-09-23 23:51:32 -0600

    Se inicia

---
+++ AlfabetoN.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def asignarN(alfabeto, palabra):
+    alfabeto = {letter: idx + 1 for idx, letter in enumerate(alfabeto)}
+    num = 0
+    for letter in palabra:
+        if letter in alfabeto:
+            num = num * len(alfabeto) + alfabeto[letter]
+    return num
+
+alfabeto = ['a', 'b','c','d']
+palabra = "dcdab"
+
+num = asignarN(alfabeto, palabra)
+print(f'La palabra "{palabra}" tiene el número {num}')
+
+def buscarP(alfabeto, num):
+    alfabeto = {idx + 1: letter for idx, letter in enumerate(alfabeto)}
+    palabra= ""
+    long = len(alfabeto)
+
+    while num > 0:
+        r = (num - 1) % long
+        palabra= alfabeto[r + 1] + palabra
+        num = (num - 1) // long
+
+    return palabra
+
+alfabeto = ['a', 'b','c','d']
+
+numero = 42
+palabra = buscarP(alfabeto, num)
+print(f'El número {numero} corresponde a la palabra "{palabra}"')
+
+
+
